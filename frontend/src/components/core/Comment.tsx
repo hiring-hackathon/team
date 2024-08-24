d43464b6c4fa79d4b5431e3f1faa96769b829a70
Faith Nchang <fnchang@montgomerycollege.edu>
2024-08-24 02:17:58 -0400

    added some comment component

---
+++ frontend/src/components/core/Comment.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState
+ } from 'react';
+ 
+export default function Comment() {
+    const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      
+    </div>
+  )
+}
