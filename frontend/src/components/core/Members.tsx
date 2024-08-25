cb59126d1f93a81f14b5c9ad81107f10b1388dea
lloydchang <lloydchang@gmail.com>
2024-08-25 00:01:09 -0700

    fix(Members.tsx): add
    fix warning in npm run build

--- frontend/src/components/core/Members.tsx
+++ frontend/src/components/core/Members.tsx
@@ -1,17 +1,18 @@
 'use client'
 import React from 'react';
+import Image from 'next/image'; // Import Image from next/image
 import { Button } from "@/components/ui/button";
 import Navbar from './Navbar';
 
 const teamMembers = [
   {
-    name: 'Shaun',
-    role: 'Backend - AWS ',
+    name: 'Shaun Jhingoor',
+    role: 'Backend - AWS',
     photo: '/path/to/Shaun.jpg',
   },
   {
-    name: 'Derek',
-    role: 'AI integration',
+    name: 'Derek Gomez',
+    role: 'AI Integration',
     photo: '/path/to/Derek.jpg',
   },
   {
@@ -20,7 +21,7 @@ const teamMembers = [
     photo: '/path/to/Lloyd.jpg',
   },
   {
-    name: 'Muturi',
+    name: 'David Muturi',
     role: 'Frontend and LLM AI Integration',
     photo: '/path/to/Muturi.jpg',
   },
@@ -31,21 +32,25 @@ const teamMembers = [
   },
 ];
 
+// Sort team members alphabetically by name
+teamMembers.sort((a, b) => a.name.localeCompare(b.name));
+
 export default function MeetTheTeam() {
   return (
-   
     <div className="p-10 bg-black">
-       <Navbar />
+      <Navbar />
       <p className="text-yellow-400 font-bold text-center text-3xl mb-8 mt-20">Meet the Team</p>
 
       <div className="p-8 bg-black">
         <div className="flex flex-wrap gap-6 justify-center">
           {teamMembers.map((member, index) => (
             <div key={index} className="bg-gray-900 p-6 text-center text-yellow-50 rounded-lg shadow-md max-w-xs flex-1">
-              <img
+              <Image
                 src={member.photo}
                 alt={`${member.name}'s photo`}
-                className="w-24 h-24 mx-auto rounded-full object-cover mb-4"
+                width={96}  // Specify width
+                height={96} // Specify height
+                className="rounded-full object-cover mb-4"
               />
               <h2 className="text-xl font-semibold mb-2 text-yellow-200">{member.name}</h2>
               <p className="text-yellow-300 mb-4">{member.role}</p>
