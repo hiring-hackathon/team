9669fdbc1b8a3c3a9e15d4c925997835d50cfc5d
lloydchang <lloydchang@gmail.com>
2024-08-24 23:19:21 -0700

    fix(utils.ts): typo

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,4 +1,4 @@
-// frontend/src/app/lib/utils/utils.ts
+// frontend/src/lib/utils/utils.ts
 
 // https://stackoverflow.com/questions/77932960/what-needs-to-be-added-to-path-lib-utils-when-tryinng-to-use-shadcn-ui-c
 
