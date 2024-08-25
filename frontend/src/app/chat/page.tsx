8cef4b045c120f1c22239b7ea1afe59f361f560c
lloydchang <lloydchang@gmail.com>
2024-08-25 15:10:38 -0700

    fix: rm page.tsx because page.js exists

--- frontend/src/app/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ChatPage() {
-    return (
-        <div className='page-container flex flex-col text-center justify-center'>
-            page
-        </div>
-    )
-}
