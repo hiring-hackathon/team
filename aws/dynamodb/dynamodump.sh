57fdf81630304dbd3ed675c590d4e4e0e7f84bd0
lloydchang <lloydchang@gmail.com>
2024-08-28 13:25:42 -0700

    feat: dyanmodb dump of current database

--- aws/dynamodb/dynamodump.sh
+++ aws/dynamodb/dynamodump.sh
@@ -7,3 +7,7 @@
 dynamodump -m backup -r us-east-1 -s Transcriptions
 
 dynamodump -m backup -r us-east-1 -s Comments
+
+find . -type f -name "*.md" | xargs git restore
+
+git status | grep deleted | awk '{print $2}' | xargs git checkout --
